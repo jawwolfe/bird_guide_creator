b9ad6bfb3b647bef8102b9e526c12733653cf907
jawwolfe <jawwolfe@gmail.com>
2021-07-26 13:40:28 -0400

    add scientific name to import

--- imort_birds.py
+++ imort_birds.py
@@ -1,4 +1,5 @@
-import pyodbc, os, glob
+from openpyxl import load_workbook
+import os, glob, pyodbc, sys
 
 path_root = 'C:/temp/'
 path_audio = path_root + 'Optimized/'
@@ -10,13 +11,46 @@ connection_string += "Trusted_Connection=yes;"
 conn = pyodbc.connect(connection_string)
 
 
+path = 'C:\\Users\\Andrew\\PycharmProjects\\audioembedder\\Clements_2019.xlsx'
+
+path_my_name = 'C:/temp/Philippines Bird Guide/'
+
+wb = load_workbook(path)
+sheetname = "eBird Clements v2019 Aug 2019"
+ws = wb[sheetname]
+
+clements_data = []
+clements_species = []
+
+for row in ws.iter_rows(min_row=2, values_only=True):
+     data = {'category': row[2], 'english': row[3], 'scientific': row[4]}
+     clements_data.append(data)
+
+for item in clements_data:
+    if item['category'] == "species":
+        bird = {'english': item['english'], 'scientific': item['scientific']}
+        clements_species.append(bird)
+
+
+def get_scientific_name(bird_name):
+    for species in clements_species:
+        if species['english'] == name:
+            return species['scientific']
+        else:
+            print('unmatched')
+            return ''
+
+
 os.chdir(path_audio)
 for file in glob.glob('*'):
     full_name = file.rsplit(".", 1)[0]
-    prefix = full_name[:2].strip()
-    name = full_name[2:].strip()
-    sql = "Insert into Birds(BirdName, TaxanomicCode) values(?,?); Select @@Identity as 'ID';"
-    params = (name, prefix)
+    prefix = full_name[:3].strip()
+    name = full_name[3:].strip()
+    scientific = get_scientific_name(name)
+    if not scientific:
+        raise ValueError('No match on common name in Clements')
+    sql = "Insert into Birds(BirdName, TaxanomicCode, ScientificName) values(?,?,?); Select @@Identity as 'ID';"
+    params = (name, prefix, scientific)
     cursor = conn.cursor()
     cursor.execute(sql, params)
     sql = "Select @@Identity as 'ID';"
