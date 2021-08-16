2b2dc9bcc4c73230c1d46766e398cb7c61cb0b85
Andrew Wolfe <awwolfe@manchester.edu>
2021-08-16 16:06:40 -0400

    Revert "Fix remove from playlist compare and add other queries"
    This reverts commit 17ba3097df29784b2a65355bd044f3131f1e810a.

--- playlist_compare.py
+++ playlist_compare.py
@@ -8,9 +8,8 @@ connection_string += "Database=BirdGuide;"
 connection_string += "Trusted_Connection=yes;"
 conn = pyodbc.connect(connection_string)
 
-
-path_playlist = "C:\\Users\\awwolfe\\PycharmProjects\\audioembedder\\Cebu Common Passerines.m3u"
-island_id = 2
+path_playlist = 'C:\\Users\\Andrew\\PycharmProjects\\audioembedder\\Panay Bird Guide.m3u'
+island_id = 10
 
 
 def connect_sqlserver():
@@ -29,10 +28,7 @@ def connect_sqlserver():
 def get_birds(island):
     conn = connect_sqlserver()
     cursor = conn.cursor()
-    #sql = "EXEC sp_get_in_island @islandid=?"
-    #sql = "EXEC sp_get_common_by_island @IslandID=?"
-    #sql = "EXEC sp_get_common_uncommon_doves_by_island @IslandID=?"
-    sql = "EXEC sp_get_common_passarines_by_island @IslandID=?"
+    sql = sql = "EXEC sp_get_in_island @islandid=?"
     params = (island)
     try:
         cursor.execute(sql, params)
@@ -56,20 +52,18 @@ for line in playlist:
         substring = line[start:end]
         prefix = substring[:3].strip()
         name = substring[3:].strip()
-        #for bird in birds:
-            #if bird[0] == prefix and bird[1] == name:
-        myline = [prefix, name]
-        playlist_birds.append(myline)
+        for bird in birds:
+            if bird[0] == prefix and bird[1] == name:
+                line = [prefix, name]
+                playlist_birds.append(line)
 
-for item in playlist_birds:
+for line in playlist_birds:
     flag = False
-    if item[1] == 'Eurasian Curlew':
-        pass
     for bird in birds:
-        if bird[0] == item[0] and bird[1] == item[1]:
+        if bird[0] == line[0] and bird[1] == line[1]:
             flag = True
     if not flag:
-        print('Remove from playlist: ' + str(item))
+        print('Remove from playlist: ' + line)
 
 for bird in birds:
     flag = False
