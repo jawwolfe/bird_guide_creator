dc6d7bbee2457cced0235c33f9c744fe3680c8ca
jawwolfe <jawwolfe@gmail.com>
2023-10-26 18:37:21 -0400

    turn on playlist refresh

--- run9_refresh_playlists.py
+++ run9_refresh_playlists.py
@@ -23,11 +23,10 @@ for super_guide in super_guides:
                                     google_cred_path=GOOGLE_CRED_PATH, super_guide_perm=SUPER_GUIDE_PERMISSIONS,
                                     drive_root='Playlists Directories')
     playlists.refresh()
-'''
+
     charts = AbundanceChartSuperGuide(logger=LOGGER, sql_server_connection=initialize_sqlserver(),
                                          chart_root=CHART_ROOT, google_api_scopes=GOOGLE_API_SCOPES,
                                          super_guide_id=super_guide[1], super_guide_name=super_guide[0],
                                          google_cred_path=GOOGLE_CRED_PATH, super_guide_perm=SUPER_GUIDE_PERMISSIONS,
                                          drive_root='Chart Directories')
     charts.refresh()
-'''
